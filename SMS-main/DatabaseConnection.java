5eb9f4cfc37150acaebb32f78a71b64f9f6303e4
Amit Kumar <amitkumarsingh843412@gmail.com>
2025-06-25 16:10:52 +0530

    Delete SMS-main directory

--- SMS-main/DatabaseConnection.java
+++
@@ -1,6 +0,0 @@
-public class DatabaseConnection {
-    // Placeholder for database logic
-    public static void connect() {
-        System.out.println("Connecting to database (not implemented)...");
-    }
-}
